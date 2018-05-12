9f97662120679ee813a6a9f4195ff8b31ffc2b4e
Tue Vu <vu.tue.1710@gmail.com>
2018-05-12 11:22:50 +0700

    first commit

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-//
+// HANDLEBARS MIDDLEWARE
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
 /** response for homepage */
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) =>
+	res.render('home', {
+		title: 'homepage'
+	})
+);
 
 /** response for about */
 app.get('/about', (req, res) => res.send(`<h1>ash</h1>`));
+
+/** response for add page */
+
+app.get('/todo/add', (req,res) => res.render('add'))
 
 app.listen(3000, () =>
 	console.log('server is connected...')
