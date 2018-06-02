ca7581329737aac012601e6b34f52681ed823853
Tue Vu <vu.tue.1710@gmail.com>
2018-06-02 14:30:07 +0700

    fix(todolist): fix db config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
-	module.exports = {
-		mongoURI:
-			'mongodb://node_todolist:adminpassword@ds143070.mlab.com:43070/node_todolist'
-	};
+		module.exports = {
+				mongoURI: 'mongodb://admintodolist:admint0d0list@ds143070.mlab.com:43070/node_todolist'
+		};
 } else {
-	module.exports = {
-		mongoURI: 'mongodb://localhost/test'
-	};
+		module.exports = {
+				mongoURI: 'mongodb://localhost/test'
+		};
 }
