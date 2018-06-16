0fe1169e651edd7a60d197781470f619651fda95
Tue Vu <just4fun11119144@gmail.com>
2018-06-16 16:37:07 +0700

    fix(feeds-board): handle error

--- routes/bullet.js
+++ routes/bullet.js
@@ -2,42 +2,60 @@ const express = require('express');
 const router = express.Router();
 const request = require('superagent');
 const mongoose = require('mongoose');
-const { differenceBy, uniq } = require('lodash');
+const {differenceBy, uniq} = require('lodash');
 let Parser = require('rss-parser');
-const { checkAuthorized } = require('../utils');
+const {checkAuthorized} = require('../utils');
 const Q = require('q');
 mongoose.Promise = require('q').Promise;
 
 require('../models/Feed');
 const Feed = mongoose.model('feed');
 
-router.get('/', async (req, res) => {
-	var parser = new Parser();
-	const rawFeeds = await parser.parseURL('https://alligator.io//feed.xml');
+router.get('/', async(req, res) => {
+  var parser = new Parser();
+  try {
+    const rawFeeds = await parser.parseURL('https://alligator.io//feed.xml')
+    const Feeds = rawFeeds
+      .items
+      .map((item) => {
+        const {
+          content = '',
+          contentSnipet = '',
+          guild = '',
+          isoDate = '',
+          link = '',
+          pubDate = '',
+          title = ''
+        } = item;
+        var category = link.split('/')[3];
+        return {
+          content,
+          contentSnipet,
+          guild,
+          isoDate,
+          link,
+          pubDate,
+          title,
+          category
+        };
+      });
+    const currFeeds = await Feed.find({});
+    const newFeeds = differenceBy(Feeds, currFeeds, 'title');
+    const storeFeed = [
+      ...currFeeds,
+      ...newFeeds
+    ];
+    const result = await Q.all(newFeeds.map((item) => new Feed(item).save()));
+  } catch (error) {
+    const result = true;
+  }
+  const finalRes = await Feed.find({});
+  const categories = uniq(finalRes.map((item) => item.category)).map((item) => ({category: item}));
 
-	const Feeds = rawFeeds.items.map((item) => {
-		const { content, contentSnipet, guild, isoDate, link, pubDate, title } = item;
-		var category = link.split('/')[3];
-		return {
-			content,
-			contentSnipet,
-			guild,
-			isoDate,
-			link,
-			pubDate,
-			title,
-			category
-		};
-	});
-	const currFeeds = await Feed.find({});
-	const newFeeds = differenceBy(Feeds, currFeeds, 'title');
-	const storeFeed = [ ...currFeeds, ...newFeeds ];
-	const result = await Q.all(newFeeds.map((item) => new Feed(item).save()));
-	const finalRes = await Feed.find({});
-	const categories = uniq(finalRes.map((item) => item.category)).map((item) => ({ category: item }));
-	if (result) {
-		res.render('board', { feeds: finalRes, categories });
-	}
+  res.render('board', {
+    feeds: finalRes,
+    categories
+  });
 });
 
 module.exports = router;
