45f40c63ddad0c0210f38603569ff1f401de7257
Tue Vu <just4fun11119144@gmail.com>
2018-06-19 22:28:17 +0700

    add termpolicy for this page

--- config/database.js
+++ config/database.js
@@ -3,15 +3,15 @@ if (process.env.NODE_ENV === 'production') {
 		mongoURI: 'mongodb://admintodolist:admint0d0list@ds143070.mlab.com:43070/node_todolist',
 		googleClientID: '673216188863-p397esh5ipjprhkedlfps5lb6s0ubklp.apps.googleusercontent.com',
 		googleClientSecret: '6oXFUSWlI0kbHukOCtlZESjZ',
-		facebookClientSecret: '248393c7ca697f9619c42635b1b25c63',
-		facebookClientID: '231287747465787'
+		facebookClientSecret: '9d9663e8bf81462141cd637b67b20167',
+		facebookClientID: '223715935095663'
 	};
 } else {
 	module.exports = {
 		mongoURI: 'mongodb://localhost/test',
 		googleClientID: '673216188863-p397esh5ipjprhkedlfps5lb6s0ubklp.apps.googleusercontent.com',
 		googleClientSecret: '6oXFUSWlI0kbHukOCtlZESjZ',
-		facebookClientSecret: '248393c7ca697f9619c42635b1b25c63',
-		facebookClientID: '231287747465787'
+		facebookClientSecret: '9d9663e8bf81462141cd637b67b20167',
+		facebookClientID: '223715935095663'
 	};
 }
